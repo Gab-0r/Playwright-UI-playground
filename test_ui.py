bd6a6c1777438048bf4a7da8446f054effb10622
gab0r <jgabriel.orozco@udea.edu.co>
2024-04-22 19:39:04 -0500

    cases added

--- test_ui.py
+++ test_ui.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page,  expect
+from playwright.sync_api import Page,  expect, TimeoutError
 from fixtures import *
 import time
 
@@ -80,3 +80,101 @@ def test_scrollbars(page: Page):
 
     btn.scroll_into_view_if_needed()
     page.screenshot(path='test-scrollbars.jpg')
+
+def test_dynamic_table(page: Page):
+    page.get_by_role("link", name="Dynamic Table").click()
+
+    label = page.locator("p.bg-warning").inner_text()
+    percentage = label.split()[-1]
+
+    colum_headers = page.get_by_role("columnheader")
+    cpu_column = None
+
+    for index in range(colum_headers.count()):
+        colum_header = colum_headers.nth(index)
+
+        if colum_header.inner_text() == "CPU":
+            cpu_column = index
+            break
+
+    assert cpu_column != None
+
+    row_group = page.get_by_role("rowgroup").last
+    chrome_row = row_group.get_by_role("row").filter(
+        has_text="Chrome"
+    )
+
+    chrome_cpu = chrome_row.get_by_role("cell").nth(cpu_column)
+    expect(chrome_cpu).to_have_text(percentage)
+
+def test_verify_test(page: Page):
+    page.get_by_role("link", name="Verify Text").click()
+
+    text = page.locator("div.bg-primary").get_by_text("Welcome", exact=False)
+
+    expect(text).to_have_text("Welcome UserName!")
+
+def test_progress_bar(page: Page):
+    page.get_by_role("link", name="Progress Bar").click()
+
+    progress_bar = page.get_by_role("progressbar")
+
+    start_btn = page.locator("button#startButton")
+    stop_btn = page.locator("button#stopButton")
+
+    start_btn.click()
+    while True:
+        progress = progress_bar.get_attribute("aria-valuenow")
+        if(int(progress) >= 75):
+            break
+        else:
+            print(f"value now is {progress}")
+
+    stop_btn.click()
+    page.screenshot(path="bar.jpg")
+    assert int(progress) >= 75
+
+def test_visibility(page: Page):
+    page.get_by_role("link", name="Visibility").click()
+    
+    hide_btn = page.get_by_role("button", name="HIde")
+    removed_btn = page.get_by_role("button", name="Removed")
+    zero_btn = page.get_by_role("button", name="Zero Width")
+    overlapped_btn = page.get_by_role("button", name="Overlapped")
+    opacity_btn = page.get_by_role("button", name="Opacity 0")
+    visibility_hide_btn = page.get_by_role("button", name="Visibility Hidden")
+    display_none_btn = page.get_by_role("button", name="Display None")
+    offscreen_btn = page.get_by_role("button", name="Offscreen")
+
+    hide_btn.click()
+
+    expect(removed_btn).to_be_hidden()
+    expect(zero_btn).to_have_css("width", "0px")
+    
+    with pytest.raises(TimeoutError):
+        overlapped_btn.click(timeout=2000)
+                             
+    expect(opacity_btn).to_have_css("opacity", "0")
+    expect(visibility_hide_btn).to_be_hidden()
+    expect(display_none_btn).to_be_hidden()
+    expect(offscreen_btn).not_to_be_in_viewport()
+
+def test_sample_app(page: Page):
+    page.get_by_role("link", name="Sample App").click()
+    user = "Will"
+    psw = "pwd"
+    
+    user_field = page.get_by_placeholder("User Name")
+    user_field.fill(user)
+
+    password_field = page.get_by_placeholder("********")
+    password_field.fill(psw)
+
+    login_btn = page.get_by_role("button", name="Log In")
+    login_btn.click()
+
+    expected_msg = f"Welcome, {user}!"
+    login_msg = page.locator("label#loginstatus")
+
+    expect(login_msg).to_have_text(expected_msg)
+
